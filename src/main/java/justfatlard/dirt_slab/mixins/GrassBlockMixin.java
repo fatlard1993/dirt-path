5788c2d383de5ed84873da3d553d3f6bf16fb2a1
fatlard1993 <fatlard1993>
2020-05-14 22:03:47 -0700

    cleanup

--- src/main/java/justfatlard/dirt_slab/mixins/GrassBlockMixin.java
+++ src/main/java/justfatlard/dirt_slab/mixins/GrassBlockMixin.java
@@ -1,25 +1,17 @@
 package justfatlard.dirt_slab.mixins;
 
 import java.util.Random;
-import java.util.List;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.GrassBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.DecoratedFeatureConfig;
-import net.minecraft.world.gen.feature.FlowerFeature;
-import net.minecraft.block.Fertilizable;
 
-import justfatlard.dirt_slab.DirtSlabBlocks;
 import justfatlard.dirt_slab.GrassSlab;
 
 @Mixin(GrassBlock.class)
